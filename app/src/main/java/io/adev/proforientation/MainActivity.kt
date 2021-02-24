67c016e2250d0efd17bfbdd9174d351702f2d137
kirill.terekhov <kirill.terekhov@alibaba-inc.com>
2021-02-24 19:18:51 +0300

    Init

---
+++ app/src/main/java/io/adev/proforientation/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.adev.proforientation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
